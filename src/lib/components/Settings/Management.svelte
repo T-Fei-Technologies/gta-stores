97991c978be5522f586db54c01e4dc7463a4c464
Erston Greatman <erston.greatman@gmail.com>
2023-10-08 18:27:45 -0600

    Version 2.0.0: Migration to PocketBase DB - Some fixes for building

--- src/lib/components/Settings/Management.svelte
+++ src/lib/components/Settings/Management.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-  import { clearStorage, loadSettings } from '$lib/appSettings/settingsHelpers';
   import MarkupAdjustments from '$lib/components/Settings/MarkupAdjustments.svelte';
   import PartsPriceList from '$lib/components/Settings/PartsPriceList.svelte';
-  import { onMount } from 'svelte';
-
-  onMount(async () => {
-    await loadSettings();
-  });
 </script>
 
 <div class="flex flex-col overflow-hidden px-0 pb-4 max-w-5xl max-sm:max-w-full max-h-full">
@@ -21,13 +15,13 @@
         <span class="text-sm">Click this if you're having issues &rarr;</span>
         <span class="text-sm text-warning">Warning: This will reset everything!</span>
       </div>
-      <button
-        type="button"
-        class="btn btn-accent text-accent-content rounded-lg ml-2 drop-shadow-2xl"
-        on:click={async () => await clearStorage(true)}
-      >
-        Clear App Storage
-      </button>
+<!--      <button-->
+<!--        type="button"-->
+<!--        class="btn btn-accent text-accent-content rounded-lg ml-2 drop-shadow-2xl"-->
+<!--        on:click={async () => await clearStorage(true)}-->
+<!--      >-->
+<!--        Clear App Storage-->
+<!--      </button>-->
     </div>
   </div>
 </div>
